e7c15ce863c8640133bccb4739d2ccc6314f9e52
Pedro Estanislado Mendez <mendezestanislado@gmail.com>
2024-04-15 16:35:45 -0600

    quitar carpeta css

--- Pagina WEB/mezcal_producto.php
+++ Pagina WEB/mezcal_producto.php
@@ -1,13 +1,12 @@
 <?php
-    require_once "conexion.php";
-    $id = "";
-    if(isset($_GET["id"]))
-    {
-        $id = $_GET["id"];
-    }
+require_once "conexion.php";
+$id = "";
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
 
-    $sql_query= "SELECT * FROM Mezcales WHERE ID_mezcal = $id";
-    $result=$conn->query($sql_query);
+$sql_query = "SELECT * FROM Mezcales WHERE ID_mezcal = $id";
+$result = $conn->query($sql_query);
 ?>
 
 
@@ -15,50 +14,48 @@
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Inicio</title>
-  <link rel="stylesheet" href="css/estilos.css" />
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Inicio</title>
+    <link rel="stylesheet" href="estilos.css" />
 </head>
 
 <body>
-  <header>
-  </header>
-  
-  <div class="contenedor-producto">
-    <?php
-        if ($result->num_rows > 0)
-        {
-            while($row = $result->fetch_assoc())
-            {
+    <header>
+    </header>
+
+    <div class="contenedor-producto">
+        <?php
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
                 ?>
-                <img src="img/<?php echo $row["Imagen_mezcal"];?>" alt="">
+                <img src="img/<?php echo $row["Imagen_mezcal"]; ?>" alt="">
                 <div class="informacion_producto">
-                    <h1><?php echo $row["Nombre_mezcal"];?></h1>
-                    <h2><?php echo $row["Tipo"];?></h2>
-                    <h3><?php echo $row["Precio"];?></h3>
-                    <p><?php echo $row["Descripcion"];?></p>
-                </div>  
+                    <h1><?php echo $row["Nombre_mezcal"]; ?></h1>
+                    <h2><?php echo $row["Tipo"]; ?></h2>
+                    <h3><?php echo $row["Precio"]; ?></h3>
+                    <p><?php echo $row["Descripcion"]; ?></p>
+                </div>
 
                 <?php
             }
         }
-        
+
         ?>
 
-    <div class="detalles">
+        <div class="detalles">
 
-        
+
         </div>
-        
-        
-    
-  </div>
 
-  <div id="chat-contenedor"></div>
-  <footer>
-  </footer>
-  <script src="js/barras de navegacion.js"></script>
+
+
+    </div>
+
+    <div id="chat-contenedor"></div>
+    <footer>
+    </footer>
+    <script src="js/barras de navegacion.js"></script>
 </body>
 
 </html>
